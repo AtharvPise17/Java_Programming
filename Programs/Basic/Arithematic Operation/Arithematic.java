24788a8fe1e74b0067fd682b0539c9912623ecde
Atharv <atharvpise16112001@gmail.com>
2022-10-31 00:00:25 +0530

    Arithematic Operation

---
+++ Programs/Basic/Arithematic Operation/Arithematic.java
@@ -0,0 +1,18 @@
+class Arithematic
+{
+public static void main(String[] args)
+{
+int a=20,b=30,sum,sub,mul,div,mod;
+sum=a+b;
+sub=a-b;
+mul=a*b;
+div=a/b;
+mod=a%b;
+System.out.println("Addition is="+sum);
+System.out.println("substraction is="+sub);
+System.out.println("Multiplication is="+mul);
+System.out.println("Division is="+div);
+System.out.println("Mod is="+mod);
+}
+}
+
