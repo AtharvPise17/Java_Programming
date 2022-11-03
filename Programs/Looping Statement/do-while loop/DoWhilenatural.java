2b7e530e967edcbc1073b063aba694321e067d7f
Atharv <atharvpise16112001@gmail.com>
2022-11-03 19:33:32 +0530

    do-while loop

---
+++ Programs/Looping Statement/do-while loop/DoWhilenatural.java
@@ -0,0 +1,13 @@
+public class DoWhilenatural 
+{    
+public static void main(String[] args) 
+{    
+    int i=1;    
+    do
+    {    
+        System.out.println(i);    
+    i++;    
+    }
+    while(i<=10);    
+}    
+}    
