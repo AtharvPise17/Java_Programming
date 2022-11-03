6909c4845040ff60e7b47fb44431694f47a09f3f
Atharv <atharvpise16112001@gmail.com>
2022-11-03 19:36:58 +0530

    for loop

---
+++ Programs/Looping Statement/for loop/fornatural.java
@@ -0,0 +1,11 @@
+//Program on prints first 10 natural number   
+public class fornatural
+{  
+public static void main(String[] args) 
+{    
+    for(int i=1;i<=10;i++)
+    {  
+        System.out.println(i);  
+    }  
+}  
+}  
